ae2793988ff7d32153c70da4daec44e0c125b9bf
RajPractiseRepo <148358152+RajPractiseRepo@users.noreply.github.com>
2024-10-21 19:49:59 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -99,7 +99,7 @@ resource "aws_eks_cluster" "demo_eks_cluster" {
 resource "aws_eks_node_group" "demo_node_group" {
   cluster_name    = aws_eks_cluster.demo_eks_cluster.name
   node_group_name = "demo-node-group"
-  node_role_arn   = aws_iam_role.demo_cluster_role.arn
+  node_role_arn   = aws_iam_role.demo_node_group_role.arn
   subnet_ids      = aws_subnet.demo_subnet[*].id
 
   scaling_config {
